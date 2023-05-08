5b047f4db0ae6e1b9cfb7e2a06c06b7983b09479
lss <lss.rp@tom.com>
2023-05-08 14:46:09 +0800

    feat: 添加 jest ts-jest 配置
    feat: 添加 eslint typescript-eslint 配置

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleNameMapper: {
+    "^package/(.*)$": "<rootDir>/package/$1",
+    "^package/": "<rootDir>/package"
+  }
 };
