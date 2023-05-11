514f6b5a2a56a9137ab77ab5daa93ab7bdd89f33
lss <lss.rp@tom.com>
2023-05-11 20:51:42 +0800

    feat: 添加 is 基础判断

--- lib/is/src/is.ts
+++ lib/is/src/is.ts
@@ -1,53 +1,117 @@
-export enum BaseTypes {
-    object = "Object",
-    array = "Array",
+export enum BaseType {
+    undefined = "Undefined",
+    null = "Null",
     string = "String",
     number = "Number",
     bool = "Boolean",
     function = "Function",
-    symbol = "Symbol"
+    date = "Date",
+    regexp = "RegExp",
+    object = "Object",
+    array = "Array",
+    symbol = "Symbol",
+    file = "File",
+    blob = "Blob",
+    set = "Set",
+    map = "Map",
+    weakset = "WeakSet",
+    weakmap = "WeakMap"
 }
 
-const localObjects = [Date, RegExp, Function, Map, Set, WeakMap, WeakSet, Blob, File]
+export type NativeSubject = ((...args: any[]) => unknown) | Date | RegExp | Set<any> | Map<any, any> | WeakSet<any> | WeakMap<any, any> | File | Blob
 
-function makeType(type: BaseTypes): string {
+function makeType(type: BaseType): string {
     return `[object ${type}]`
 }
 
-function typeOf(target: any) {
+function isTypeOf(target: any): string {
     return Object.prototype.toString.call(target)
 }
 
-function is(target: any, type: BaseTypes) {
-    return typeOf(target) === makeType(type)
+function is(target: any, type: BaseType): boolean {
+    return isTypeOf(target) === makeType(type)
 }
 
-function isObject(target: any): target is object {
-    return is(target, BaseTypes.object)
-}
-
-function isArray(target: any): target is Array<any> {
-    return is(target, BaseTypes.array)
+function checkAction(target: any, action: ((target: any) => boolean)[]): boolean {
+    return action.reduce((res, current) => {
+        return res && current(target)
+    }, true)
 }
 
 function isString(target: any): target is string {
-    return is(target, BaseTypes.string)
+    return is(target, BaseType.string)
 }
 
 function isNumber(target: any): target is number {
-    return is(target, BaseTypes.number) && !isNaN(target)
+    return is(target, BaseType.number) && !isNaN(target)
 }
 
 function isBool(target: any): target is boolean {
-    return is(target, BaseTypes.bool)
+    return is(target, BaseType.bool)
+}
+
+function isNativeBase(target: any): target is string | number | boolean {
+    return isString(target) || isNumber(target) || isBool(target)
 }
 
 function isFunction(target: any): target is ((...args: any[]) => unknown) {
-    return is(target, BaseTypes.function) || typeof target === "function"
+    return is(target, BaseType.function) || typeof target === "function"
+}
+
+function isDate(target: any): target is Date {
+    return is(target, BaseType.date)
+}
+
+function isRegExp(target: any): target is RegExp {
+    return is(target, BaseType.regexp)
+}
+
+function isNativeObject(target: any): target is ((...args: any[]) => unknown) | Date | RegExp {
+    return checkAction(target, [isFunction, isDate, isRegExp])
 }
 
 function isSymbol(target: any): target is symbol {
-    return is(target, BaseTypes.symbol)
+    return is(target, BaseType.symbol)
+}
+
+function isObject(target: any): target is object {
+    return is(target, BaseType.object)
+}
+
+function isArray(target: any): target is Array<any> {
+    return is(target, BaseType.array)
+}
+
+function isSet(target: any): target is Set<any> {
+    return is(target, BaseType.set)
+}
+
+function isMap(target: any): target is Map<any, any> {
+    return is(target, BaseType.map)
+}
+
+function isWeakSet(target: any): target is WeakSet<any> {
+    return is(target, BaseType.weakset)
+}
+
+function isWeakMap(target: any): target is WeakMap<any, any> {
+    return is(target, BaseType.weakmap)
+}
+
+function isNativeSetMap(target: any): target is Set<any> | Map<any, any> | WeakSet<any> | WeakMap<any, any> {
+    return checkAction(target, [isSet, isMap, isWeakSet, isWeakMap])
+}
+
+function isFile(target: any): target is File {
+    return is(target, BaseType.file)
+}
+
+function isBlob(target: any): target is Blob {
+    return is(target, BaseType.blob)
+}
+
+function isNativeFile(target: any): target is Blob | File {
+    return checkAction(target, [isFile, isBlob])
 }
 
 function isPromise(target: any): target is Promise<any> {
@@ -55,6 +119,7 @@ function isPromise(target: any): target is Promise<any> {
 }
 
 function isNil(target: any): target is undefined | null {
+    // isNaN({ a: "safds" }) returns true
     return target === undefined || target === null
 }
 
@@ -62,9 +127,13 @@ function isNilEmpty(target: any): target is undefined | null | "" {
     return isNil(target) || target === ""
 }
 
+function isNativeSubject(target: any): target is NativeSubject {
+    return checkAction(target, [isFunction, isDate, isRegExp, isSet, isMap, isWeakSet, isWeakMap, isBlob, isFile])
+}
+
 function isNilObject(target: any): target is object {
     if (!isObject(target) && !isArray(target)) return isNilEmpty(target)
-    if (target.constructor && localObjects.includes(target.constructor as any)) return false
+    if (target.constructor && isNativeSubject(target)) return false
     if (isArray(target)) return target.length < 1
     for (const entry of Object.entries(target)) {
         const [, value] = entry
@@ -73,16 +142,55 @@ function isNilObject(target: any): target is object {
     return true
 }
 
-is.object = isObject
-is.array = isArray
+function isBufferSource(target: any): target is BufferSource {
+    return target instanceof ArrayBuffer ||
+        target instanceof Int8Array ||
+        target instanceof Uint8Array ||
+        target instanceof Uint8ClampedArray ||
+        target instanceof Int16Array ||
+        target instanceof Uint16Array ||
+        target instanceof Int32Array ||
+        target instanceof Uint32Array ||
+        target instanceof Float32Array ||
+        target instanceof Float64Array ||
+        target instanceof BigInt64Array ||
+        target instanceof BigUint64Array ||
+        target instanceof DataView
+}
+
+function isBodyInit(target: any): target is BodyInit {
+    return target instanceof FormData ||
+        target instanceof URLSearchParams ||
+        isBufferSource(target) ||
+        target instanceof Blob ||
+        target instanceof ReadableStream
+}
+
+is.typeOf = isTypeOf
 is.string = isString
 is.number = isNumber
 is.bool = isBool
 is.function = isFunction
+is.date = isDate
+is.regExp = isRegExp
+is.object = isObject
 is.symbol = isSymbol
+is.array = isArray
+is.set = isSet
+is.map = isMap
+is.weakSet = isWeakSet
+is.weakMap = isWeakMap
+is.file = isFile
+is.blob = isBlob
 is.promise = isPromise
 is.nil = isNil
 is.nilEmpty = isNilEmpty
 is.nilObject = isNilObject
+is.nativeBase = isNativeBase
+is.nativeObject = isNativeObject
+is.nativeFile = isNativeFile
+is.nativeSubject = isNativeSubject
+is.nativeSetMap = isNativeSetMap
+is.bodyInit = isBodyInit
 
 export default is
